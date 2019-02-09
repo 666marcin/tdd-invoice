c508c73d9c6b6e9979a880d7445e27475a413be3
student7 <student7@S7N-PROGLANG-43.pcoip.ki.agh.edu.pl>
2019-02-09 15:47:57 +0100

    poprawic petle subtotal

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -8,22 +8,25 @@ import java.util.HashMap;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private HashMap <Product, Integer> products = new HashMap <>();
-	private BigDecimal subtotal;
+	protected HashMap<Product, Integer> products = new HashMap<>();
+	
 
 	public void addProduct(Product product) {
-		this.products.put(product, 1);
+		this.addProduct(product,1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
 		this.products.put(product, quantity);
-		}
+	}
 
+	
+	
 	public BigDecimal getSubtotal() {
-//		for (Product product : this.products) {
-//
-//		}
-		return BigDecimal.ZERO;
+		BigDecimal subtotal = BigDecimal.ZERO;
+		for (Product product : products.keySet()) {
+			//BigDecimal productPrice = (product.getPrice()).multiply(products(product).values);
+		}
+		return subtotal;
 	}
 
 	public BigDecimal getTax() {
